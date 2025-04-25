388419182e937efea74e289519944df8bdfa1ff2
kiudev <dani.97.saavedra@gmail.com>
2025-04-25 22:14:31 +0200

    update CORS configuration to allow requests from the correct client URL

--- showly-server/api/config/cors.ts
+++ showly-server/api/config/cors.ts
@@ -1,7 +1,8 @@
 import cors from "cors";
 
 const corsOptions: cors.CorsOptions = {
-  origin: ["http://localhost:5173", "https://showly-server.vercel.app"],
+  origin: ["http://localhost:5173", "https://showly-client.vercel.app"],
+  methods: ["GET", "POST", "PATCH", "DELETE"],
   credentials: true,
 };
 
