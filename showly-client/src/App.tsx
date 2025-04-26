8b2cbb4119a8abb5bc56c4ca55019b7e084da1e8
kiudev <dani.97.saavedra@gmail.com>
2025-04-26 11:31:47 +0200

    add header title

--- showly-client/src/App.tsx
+++ showly-client/src/App.tsx
@@ -4,6 +4,9 @@ import { Main } from "./pages/Main";
 function App() {
   return (
     <div className="flex flex-col justify-center items-center max-w-full min-h-full absolute inset-0 bg-black">
+      <header className="absolute z-1 top-10">
+        <h1 className="uppercase text-white tracking-widest text-5xl">Showly</h1>
+      </header>
       <Main />
     </div>
   );
