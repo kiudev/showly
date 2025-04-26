5c299b12cef0a5b774b63216942b2a88ff0928cc
kiudev <dani.97.saavedra@gmail.com>
2025-04-26 11:32:49 +0200

    remove border from main container

--- showly-client/src/pages/Main.tsx
+++ showly-client/src/pages/Main.tsx
@@ -2,7 +2,7 @@ import { Hero } from "@/components/main/Hero";
 
 export const Main = () => {
   return (
-    <main className="flex flex-col gap-5 items-center border w-full h-full">
+    <main className="flex flex-col gap-5 items-center w-full h-full">
       <Hero />
     </main>
   );
